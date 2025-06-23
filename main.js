727ab667ae8f50ac6da40968a4b89c48b5b20c49
duaali786786 <duanaval64@gmail.com>
2025-06-23 19:59:31 +0700

    h

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-var choice = prompt("welcome \n 1.area of rectangle \n 2.area of triangle \n 3.area of cirlce \n 4. area of parallogram")
+var choice = prompt("welcome \n 1.perimeter of rectangle \n 2.perimeter of triangle \n 3.perimeter of pentagon \n 4.perimeter of hexagon  ")
 
 if (choice == "1") {
-    var a = prompt("enter your height")
-    var b = prompt("enter your base")
-    var result= Number(a)*Number(b)
+    var a = prompt("enter the length of first side")
+    var b = prompt("enter the length of second side")
+    var c = prompt("enter the length of third side")
+    var d = prompt("enter the length of forth side")
+    var result= Number(a)+Number(b)+Number(c)+Number(d)
     alert("the result is "+ result)
 }
 if (choice == "2") {
-    var c = prompt("enter your height")
-    var d = prompt("enter your base")
-    var result= Number(c)*Number(d)*0.5
+   var e = prompt("enter the length of first side")
+    var f = prompt("enter the length of second side")
+    var g = prompt("enter the length of third side")
+    var result= Number(e)+Number(f)+Number(g)
     alert("the result is "+ result)
 }
 if (choice == "3") {
-    var e = prompt("enter your radius")
-    var result= 3.14*Number(e)*Number(e)
+    var h = prompt("enter the length of first side")
+    var i = prompt("enter the length of second side")
+    var j = prompt("enter the length of third side")
+    var k = prompt("enter the length of forth side")
+    var l = prompt("enter the length of fifth side")
+     var result= Number(h)+Number(i)+Number(j)+Number(k)+Number(l)
     alert("the result is "+ result)
 }
-if (choice == "4") {
-    var f = prompt("enter your height")
-    var g = prompt("enter your base")
-    var result= Number(f)*Number(g)
+if (choice == "4") {                                            
+    var m = prompt("enter the length of first side")
+    var n = prompt("enter the length of second side")
+    var o = prompt("enter the length of third side")
+    var p = prompt("enter the length of forth side")
+    var q = prompt("enter the length of fifth side")
+    var r = prompt("enter the length of sixth side")
+     var result= Number(m)+Number(n)+Number(o)+Number(p)+Number(q)+Number(r)
     alert("the result is "+ result)
+
 }
